f1b8af812f69f11ac0b638d8fbaecc9fef17f866
cihatarass <cihataras95@gmail.com>
2022-10-10 01:56:07 +0300

    Proje dosyası ekle.

---
+++ TestJquery/Controllers/KimliksController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestJquery.Models;
+
+namespace TestJquery.Controllers
+{
+    public class KimliksController : Controller
+    {
+        private JqueryTestEntities db = new JqueryTestEntities();
+
+        // GET: Kimliks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult Veriler()
+        {
+            var veriler = db.Kimlik.ToList();
+
+            return Json(
+                new
+                {
+                    Result = from obj in veriler
+                             select new
+                             {
+                                 obj.id,
+                                 obj.ad,
+                                 obj.soyad,
+                                 obj.no,
+                             }
+                }, JsonRequestBehavior.AllowGet
+                );
+        }
+
+        public JsonResult EkleJson(string ad, string soyad, string no)
+        {
+            Kimlik kmlk = new Kimlik();
+            kmlk.ad = ad;
+            kmlk.soyad = soyad;
+            kmlk.no = no;
+            db.Kimlik.Add(kmlk);
+            var durum = db.SaveChanges();
+            if (durum > 0)
+                return Json("1");
+            else return Json("0");
+
+
+        }
+
+        [HttpPost]
+        public JsonResult SilJson(int[] data)
+        {
+            foreach (var id in data)
+            {
+                var kimlik = db.Kimlik.FirstOrDefault(x=>x.id == id);
+                db.Kimlik.Remove(kimlik);
+            }
+            var durum = db.SaveChanges();
+            if (durum > 0)
+                return Json("1");
+            else return Json("0");
+        }
+
+        [HttpPost]
+        public JsonResult GuncelleJson(string id)
+        {
+            var ID = Convert.ToInt32(id);
+            var veri = db.Kimlik.FirstOrDefault(x=>x.id == ID);
+
+            return Json(
+                new
+                {
+                    Result = new
+                             {
+                                 veri.id,
+                                 veri.ad,
+                                 veri.soyad,
+                                 veri.no,
+                             }
+                }, JsonRequestBehavior.AllowGet
+                );
+
+        }
+
+
+        [HttpPost]
+        public JsonResult Guncelle(string id, string ad, string soyad, string no)
+        {
+            var ID = Convert.ToInt32(id);
+            var kmlk = db.Kimlik.FirstOrDefault(x => x.id == ID);
+            kmlk.ad = ad;
+            kmlk.soyad = soyad;
+            kmlk.no = no;
+            var durum = db.SaveChanges();
+            if (durum > 0)
+                return Json("1");
+            else return Json("0");
+
+
+        }
+
+        // GET: Kimliks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kimlik kimlik = db.Kimlik.Find(id);
+            if (kimlik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kimlik);
+        }
+
+        // GET: Kimliks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kimliks/Create
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,ad,soyad,no")] Kimlik kimlik)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kimlik.Add(kimlik);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kimlik);
+        }
+
+        // GET: Kimliks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kimlik kimlik = db.Kimlik.Find(id);
+            if (kimlik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kimlik);
+        }
+
+        // POST: Kimliks/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,ad,soyad,no")] Kimlik kimlik)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kimlik).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kimlik);
+        }
+
+        // GET: Kimliks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kimlik kimlik = db.Kimlik.Find(id);
+            if (kimlik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kimlik);
+        }
+
+        // POST: Kimliks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kimlik kimlik = db.Kimlik.Find(id);
+            db.Kimlik.Remove(kimlik);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TestJquery/Scripts/kimlik.js
@@ -0,0 +1,186 @@
+﻿function Veriler() {
+    var url = '/Kimliks/Veriler';
+    $('table').html("");
+    var thead = '<thead><tr><td>Seç</td><td>AD</td><td>Soyad</td><td>no</td></tr></thead>';
+    $('table').append(thead);
+    $.getJSON(url, function (data) {
+        for (var item in data.Result) {
+            var guncelle = '<button data-id='+data.Result[item].id+' class="btn btn-primary guncelle">Güncelle</button>';
+            var checkbox = '<input type="checkbox" name="secilmis" value=' + data.Result[item].id + ' />';
+            var deger = '<tbody><tr><td>' + checkbox + '</td><td>' + data.Result[item].ad + '</td><td>' + data.Result[item].soyad + '</td><td>' + data.Result[item].no + '</td> <td>' + guncelle + '</td></tr></tbody>';
+            $('table').append(deger);
+        }
+    });
+}
+
+$(document).on("click", "#temizle", function () {
+    $('table').html("");
+});
+
+$(document).on("click", "#yenile", function () {
+    Veriler();
+});
+
+$(document).on("click", "#ekle", async function () {
+    const { value: formValues } = await Swal.fire({
+        title: 'Veri Ekle',
+        showCancelButton: true,
+        cancelButtonColor: '#d43f3a',
+        cancelButtonText: "İptal",
+        confirmButtonText: "Ekle",
+        confirmButtonColor: '#337ab7',
+        html:
+            '<input id="ad" placeholder="Ad Giriniz" class="swal2-input">' +
+            '<input id="soyad" placeholder="Soyad Giriniz" class="swal2-input">' +
+            '<input id="no" placeholder="Telefon No Giriniz" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+            return [
+                document.getElementById('ad').value,
+                document.getElementById('soyad').value,
+                document.getElementById('no').value
+            ]
+        }
+    })
+
+    $.ajax({
+
+        url: '/Kimliks/EkleJson',
+        type: 'Post',
+        dataType: 'Json',
+        data: { ad: formValues[0], soyad: formValues[1], no: formValues[2] },
+        success: function (gelenDeg) {
+            if (gelenDeg == '1') {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Başarılı',
+                    text: 'Kayıt başarıyle eklendi.',
+                })
+                Veriler();
+            }
+
+            else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Hata!',
+                    text: 'Bir hata oluştu ve kayıt eklenemedi.',
+                })
+            }
+        }
+    });
+
+});
+
+$(document).on("click", "#sil", function () {
+    Swal.fire({
+        title: 'Silmek İstediğinize Emin misiniz?',
+        text: "Silinen veriler tekrar geri getirelemeyektir.",
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonText:'İptal',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Evet, Sil!'
+    }).then((result) => {
+        if (result.value) {
+            var data = [];
+            var tr = [];
+            var sayac = 0;
+            $("input[name='secilmis']:checked").each(function () {
+                data[sayac] = $(this).val();
+                tr[sayac] = $(this).parent('td').parent('tr');
+                sayac++;
+
+            });
+            $.ajax({
+                url: '/Kimliks/SilJson',
+                type: 'Post',
+                dataType: 'Json',
+                data: { "data": data },
+                success: function (gelenDeg) {
+                    if (gelenDeg == '1') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Silindi!',
+                            text: 'Kayıtlar başarıyla Silindi.',
+                        })
+                        var sayac2 = 0;
+                        $(tr).each(function () {
+                            tr[sayac2].remove();
+                            sayac2++;
+                        })
+                    }
+
+                    else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Hata!',
+                            text: 'Bir hata oluştu ve kayıt silinemedi eklenemedi.',
+                        })
+                    }
+                }
+            })
+        }
+    })
+});
+
+
+
+$(document).on("click", ".guncelle", async function () {
+    var id = $(this).attr('data-id');
+    $.ajax({
+
+        url: '/Kimliks/GuncelleJson',
+        type: 'Post',
+        dataType: 'Json',
+        data: { 'id': id },
+        success: async function (data) {
+            const { value: formValues } = await Swal.fire({
+                title: 'Veri Ekle',
+                showCancelButton: true,
+                cancelButtonColor: '#d43f3a',
+                cancelButtonText: "İptal",
+                confirmButtonText: "Ekle",
+                confirmButtonColor: '#337ab7',
+                html:
+                    '<input id="ad" value=' + data.Result.ad + ' placeholder="Ad Giriniz" class="swal2-input">' +
+                    '<input id="soyad" value=' + data.Result.soyad + ' placeholder="Soyad Giriniz" class="swal2-input">' +
+                    '<input id="no" value=' + data.Result.no + ' placeholder="Telefon No Giriniz" class="swal2-input">',
+                focusConfirm: false,
+                preConfirm: () => {
+                    return [
+                        document.getElementById('ad').value,
+                        document.getElementById('soyad').value,
+                        document.getElementById('no').value
+                    ]
+                }
+            })
+
+            $.ajax({
+
+                url: '/Kimliks/Guncelle',
+                type: 'Post',
+                dataType: 'Json',
+                data: { id: id, ad: formValues[0], soyad: formValues[1], no: formValues[2] },
+                success: function (gelenDeg) {
+                    if (gelenDeg == '1') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Başarılı',
+                            text: 'Kayıt başarıyle Güncellendi.',
+                        })
+                        Veriler();
+                    }
+
+                    else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Hata!',
+                            text: 'Bir hata oluştu ve kayıt güncellenemedi.',
+                        })
+                    }
+                }
+            });
+        }
+    });
+});
